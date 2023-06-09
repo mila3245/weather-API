1dec057bd4c3bfaace0f21d964123f9ba3683880
Kamila Swiader <kamila_swiader93@hotmail.com>
2023-06-09 09:27:02 +0100

    search history

--- script.js
+++ script.js
@@ -8,3 +8,44 @@ var queryURL= "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={l
     console.log(queryURL);
     console.log(response);
 }) */
+
+
+var searchbuttonaEl = $("#search-button");
+var searchInputEl = $("#search-input");
+var historyEl = $("#history");
+var searchFormEl = $("#search-form");
+
+
+searchFormEl.on("submit", function (event) {
+    event.preventDefault();
+    var city = searchInputEl.val()
+
+    var history = JSON.parse(localStorage.getItem("history")) || [];
+
+    if (!history.includes(city)) {
+        history.push(city);
+        localStorage.setItem("history", JSON.stringify(history))
+        displayHistoryButtons()
+    }
+
+});
+
+function displayHistoryButtons() {
+    historyEl.empty();
+
+    var history = JSON.parse(localStorage.getItem("history")) || [];
+
+    for (var i = 0; i < history.length; i++) {
+        var cityName = history[i];
+        var buttonHTML = `
+        <button type="button" class="btn city-button btn-primary mt-3">
+            ${cityName}
+        </button>
+        `
+        historyEl.append(buttonHTML);
+    }
+}
+
+
+
+displayHistoryButtons()
