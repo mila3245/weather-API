32fe8429017fb6b396f502b008be63fb4326511c
Kamila Swiader <kamila_swiader93@hotmail.com>
2023-06-09 09:22:27 +0100

    updated layout

---
+++ script.js
@@ -0,0 +1,10 @@
+var APIKey= "c9cb15b4beb7e2eed4a1b78777b577d7";
+var queryURL= "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}" + APIKey;
+
+/* $.ajax ({
+    url:queryURL,
+    method:"GET"
+}).then(function(response) {
+    console.log(queryURL);
+    console.log(response);
+}) */
